f948e3788a89a29deb660861e7dd9563f09c07e1
ManuAchar <106185012+MAchar1@users.noreply.github.com>
2022-10-04 23:25:59 -0400

    Create ThingsThatMove.java

---
+++ ThingsThatMove.java
@@ -0,0 +1,22 @@
+package Chapter3HW;
+
+public class ThingsThatMove {
+    public static void main(String[] args) {
+        Airplane a = new Airplane("Boeing 747", 2016);
+        Bird b1 = new Bird("Eagle");
+        Bird b2 = new Bird("Hummingbird");
+
+        System.out.println(a);
+        a.fly();
+        a.walk();
+        a.jump();
+        System.out.println(b1);
+        b1.fly();
+        b1.walk();
+        b1.jump();
+        System.out.println(b2);
+        b2.fly();
+        b2.walk();
+        b2.jump();
+    }
+}
